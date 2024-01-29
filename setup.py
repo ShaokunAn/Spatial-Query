f8b83474d597afd5fbc7f478a1cad5e88913efc1
sa3520 <shaokunan1@gmail.com>
2024-01-29 12:23:44 -0500

    use find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='SpatialQuery',
     version='0.1',
-    packages=[''],
+    packages=find_packages(),
     url='https://github.com/ShaokunAn/Spatial-Query',
     license='MIT',
     author='Shaokun An',
