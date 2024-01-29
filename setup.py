c70784d2d386443a6f10919d56b904bffee44e99
sa3520 <shaokunan1@gmail.com>
2024-01-29 10:41:28 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SpatialQuery',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/ShaokunAn/Spatial-Query',
+    license='MIT',
+    author='Shaokun An',
+    author_email='shan12@bwh.harvard.edu',
+    description=''
+)
