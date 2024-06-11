99ba73a4b90076fa54f8cd8988bb6db4c430b55f
sa3520 <shaokunan1@gmail.com>
2024-06-10 22:11:46 -0400

    remove cpp extensions

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+import pybind11
 
 setup(
     name='SpatialQuery',
     version='0.1',
     packages=find_packages(),
-    url='https://github.com/ShaokunAn/Spatial-Query',
+    # url='https://github.com/ShaokunAn/Spatial-Query',
     license='MIT',
     author='Shaokun An',
     author_email='shan12@bwh.harvard.edu',
-    description=''
+    description='',
 )
