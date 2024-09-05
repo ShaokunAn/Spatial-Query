497579ac1982f40ed03d374c225ddfa68ddc9555
sa3520 <shaokunan1@gmail.com>
2024-09-05 16:36:17 -0400

    update comments

--- SpatialQuery/spatial_query.py
+++ SpatialQuery/spatial_query.py
@@ -48,7 +48,6 @@ class spatial_query:
 
         grid_cell_types = {}
         grid_indices = {}
-        print(f"self.overlap_radius: {self.overlap_radius}")
 
         for i in range(10):
             for j in range(10):
